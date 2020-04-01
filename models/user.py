2b2baacaf4b0a8126a9196cab165c3fbcb5422cb
auraPasmin <1288@holbertonschool.com>
2020-04-01 01:15:18 -0500

    add user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.review import Review
+from models.place import Place
 
 
 class User(BaseModel):
@@ -11,7 +15,12 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
