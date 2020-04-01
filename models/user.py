c426f272ec2a034a3e3a3c1491afb5fcad2e1228
auraPasmin <1288@holbertonschool.com>
2020-04-01 02:52:33 -0500

    original user

--- models/user.py
+++ models/user.py
@@ -15,10 +15,7 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-
-    __tablename__ = "users"
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
