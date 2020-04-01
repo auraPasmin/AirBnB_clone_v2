9f8546ddac11f3d75c99f605929413d58175a2cd
auraPasmin <1288@holbertonschool.com>
2020-04-01 02:32:38 -0500

    fix file storage and base

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,27 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+     id = Column(String(60),
+                primary_key=True,
+                nullable=False)
+
+    created_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
+
+    updated_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
