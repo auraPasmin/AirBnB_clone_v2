4db19df352fddb2ef7be35b0bcf21f751c98cd22
auraPasmin <1288@holbertonschool.com>
2020-04-01 12:43:24 -0500

    fix file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,4 +69,3 @@ class FileStorage:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
-        self.save()
