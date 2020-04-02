2baeaf94bd39692cf8abec2ffc34246e209e54fe
JuanDavidDava2 <juandm417@gmail.com>
2020-04-02 06:40:01 +0000

    add task 6 whit all its files

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,7 @@ import uuid
 import models
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalcemy.ext.declarative import  declarative_base
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -14,11 +13,11 @@ class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
 """
-    id = Column(String(60),primary_key=True,nullable=False)
+    id = Column(String(60), primary_key=True, nullable=False)
 
-    created_at = Column(DateTime,nullable=False,default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    updated_at = Column(DateTime,nullable=False,default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -36,15 +35,21 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__" and hasattr(str, key):
                     setattr(self, key, value)
-            if 
+            if self.id is None:
+                setattr(self, 'id', str(uuid.uuid4()))
+            time = datetime.now()
+            if self.created_at is None:
+                self.created_at = time
+            if self.updated_at is None:
+                self.updated_at = time
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-        
+
     def __str__(self):
         """returns a string
         Return:
-            returns a string of class name, id, and dictionary
+        returns a string of class name, id, and dictionary
         """
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
@@ -71,3 +76,8 @@ class BaseModel:
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    def delete(self):
+        """delete the current instance
+        """
+        models.storage.delete(self)
