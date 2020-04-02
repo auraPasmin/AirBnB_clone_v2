dbe4094901d2296e0ae57a7b801807f023b2fe7b
JuanDavidDava2 <juandm417@gmail.com>
2020-04-02 01:16:54 +0000

    create Base = declarative

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,22 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalcemy.ext.declarative import  declarative_base
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
-    """
+"""
+    id = Column(String(60),primary_key=True,nullable=False)
+
+    created_at = Column(DateTime,nullable=False,default=datetime.utcnow())
+
+    updated_at = Column(DateTime,nullable=False,default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -24,13 +34,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                if key != "__class__" and hasattr(str, key):
                     setattr(self, key, value)
+            if 
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
-
+        
     def __str__(self):
         """returns a string
         Return:
@@ -48,6 +58,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
