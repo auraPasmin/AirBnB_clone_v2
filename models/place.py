1cb1fd3a71992c9532f4a28ddef1f032231be2fd
JuanDavidDava2 <juandm417@gmail.com>
2020-04-03 02:01:01 +0000

    add comments

--- models/place.py
+++ models/place.py
@@ -10,17 +10,17 @@ from os import getenv
 class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
-        city_id: city id
-        user_id: user id
-        name: name input
-        description: string of description
-        number_rooms: number of room in int
-        number_bathrooms: number of bathrooms in int
-        max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
-        longitude: longitude in float
-        amenity_ids: list of Amenity ids
+        city_id: city id.
+        user_id: user id.
+        name: name input.
+        description: string of description.
+        number_rooms: number of room in int.
+        number_bathrooms: number of bathrooms in int.
+        max_guest: maximum guest in int.
+        price_by_night:: pice for a staying in int.
+        latitude: latitude in flaot.
+        longitude: longitude in float.
+        amenity_ids: list of Amenity ids.
     """
     __tablename__ = "places"
 
