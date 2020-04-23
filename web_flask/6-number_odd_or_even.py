b11de3974eba11d4a213c44f8b35ab095354d406
auraPasmin <1288@holbertonschool.com>
2020-04-22 20:58:02 -0500

    add 6 permissions ok pep8 ok

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,9 +45,11 @@ def if_number(n):
 def if_html(n):
     return render_template("5-number.html", n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
